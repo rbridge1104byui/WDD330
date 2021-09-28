d2f0ecd43a17a029d094c3e811ad85baf4c78a77
rbridge1104 <35268078+rbridge1104@users.noreply.github.com>
2021-09-28 12:30:07 -0400

    Updated all weeks and got JS functionality working

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
+
+
 const links = [
     {
-        name: "Week1 notes",
-        url: "week1/index.html"
+      label: "Week-01 ",
+      url: "Week01/index.html",
+    },
+    {
+      label: "Week-02 ",
+      url: "Week02/index.html",
+    },
+    {
+      label: "Week-03 ",
+      url: "Week03/index.html",
+    },
+  ];
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    var ol = document.getElementById("exercise_list");
+  
+    for (var i = 0; i < links.length; i++) {
+      var link = links[i];
+      var a = document.createElement("a");
+      var li = document.createElement("li");
+      a.textContent = link.label;
+      a.setAttribute("href", link.url);
+      li.appendChild(a);
+      ol.appendChild(li);
     }
-]
-function list (links) {
-    for (var link in links) {
-        document.getElementById('list-of-links').innerHTML += '<li>' + link + '</li>';
-    }
-}
+  });
